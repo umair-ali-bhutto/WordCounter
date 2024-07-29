5245b3fecd24f283d075ff208d75f26f8b166549
umair-ali-bhutto <umair2101f@aptechgdn.net>
2024-07-29 18:20:17 +0500

    Added Light Mode And Dark Mode Functionality

--- script.js
+++ script.js
@@ -2,13 +2,16 @@
  * @author umair-ali-bhutto
  * Simple Javascript Project For Counting Characters, Words And Lines 
  */
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const textArea = document.getElementById('textArea');
     const characterCountSpan = document.getElementById('characterCount');
     const wordCountSpan = document.getElementById('wordCount');
     const lineCountSpan = document.getElementById('lineCount');
+    const modeToggle = document.getElementById('modeToggle');
+    const toggleLabel = document.getElementById('toggleLabel');
+    const body = document.body;
 
-    textArea.addEventListener('input', function() {
+    textArea.addEventListener('input', function () {
         const text = textArea.value;
         const characterCount = text.length;
         const wordCount = text.trim().split(/\s+/).length;
@@ -17,5 +20,15 @@ document.addEventListener('DOMContentLoaded', function() {
         characterCountSpan.textContent = characterCount;
         wordCountSpan.textContent = wordCount;
         lineCountSpan.textContent = lineCount;
+    });
+
+    modeToggle.addEventListener('change', function () {
+        if (modeToggle.checked) {
+            body.classList.add('light-mode');
+            toggleLabel.textContent = 'Light Mode';
+        } else {
+            body.classList.remove('light-mode');
+            toggleLabel.textContent = 'Dark Mode';
+        }
     });
 });
