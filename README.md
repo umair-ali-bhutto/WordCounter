a00ec3780e994edffa7a1a145dd4a8a305077b4c
umair-ali-bhutto <umair2101f@aptechgdn.net>
2024-07-29 18:04:25 +0500

    Added Files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # WordCounter
 Simple Character, Words And Line Counter Using JS
+
+# Live Demo
+
+- deployed on github [https://umair-ali-bhutto.github.io/WordCounter/](https://umair-ali-bhutto.github.io/WordCounter/)
+
---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const textArea = document.getElementById('textArea');
+    const characterCountSpan = document.getElementById('characterCount');
+    const wordCountSpan = document.getElementById('wordCount');
+    const lineCountSpan = document.getElementById('lineCount');
+
+    textArea.addEventListener('input', function() {
+        const text = textArea.value;
+        const characterCount = text.length;
+        const wordCount = text.trim().split(/\s+/).length;
+        const lineCount = text.split('\n').length;
+
+        characterCountSpan.textContent = characterCount;
+        wordCountSpan.textContent = wordCount;
+        lineCountSpan.textContent = lineCount;
+    });
+});
